c824dde3f66ccaf79e15d42e29ddf2541430c999
derfelot <derfelot.git@gmail.com>
2020-12-30 08:55:18 +0100

    poplar_canada: Import modem configurations from stock
    And place them into /system/etc/customization/modem/modem.conf and /system/etc/customization/modem/[custid]/modem.conf
    
    This is the fallback location for both the OpenCustomizationSelector and sony-modem-switcher.
    
    Taken from stock /oem.

--- device/config-files.mk
+++ device/config-files.mk
@@ -2,3 +2,10 @@
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
     $(DEVICE_PATH)/config/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+
+### Modem Configs
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/config/modem/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/modem.conf \
+    $(DEVICE_PATH)/config/modem/287/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/287/modem.conf \
+    $(DEVICE_PATH)/config/modem/409/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/409/modem.conf \
+    $(DEVICE_PATH)/config/modem/451/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/451/modem.conf
