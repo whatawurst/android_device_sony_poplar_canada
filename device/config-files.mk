298269f09f5a4bbaf53369de63cbcfa25bcba42c
derfelot <derfelot.git@gmail.com>
2020-12-30 08:57:55 +0100

    poplar_canada: Add missing modem config and assign new custid
    These are taken from the carrier specific fw oem partition and are usually handled by being the default modem conf. Since the default for us is the standard modem, assign a custid to them and import the modem with this custid. Use relative high custid's to avoid clashing with some that might already be assigned by Sony, but missing from our list.
    
    Across yoshino, this concerns the following:
    
    mcc  mnc  modem                        simid              custid  devices                 rom
    230  01   telekom_czech_volte_vowifi   S175.1             901     lilac                   T-Mobile CZ
    272  05   hutch_ireland                S9901.1            902     poplar                  3 IE
    202  01   telekom_greece_volte_vowifi  S273.1             903     lilac / maple / poplar  Cosmote GR
    216  30   telekom_hungary              S180.1             904     maple                   T-Mobile HU
    216  70   vodafone_hungary             S181.1             905     maple / poplar          Vodafone HU
    219  01   telekom_croatia              S176.1             906     lilac / maple / poplar  T-Mobile
    219  10   vipnet_croatia               S9902.1            907     maple                   Vipnet HR
    226  01   vodafone4                    S228.1             908     maple / poplar          Vodafone RO
    230  03   vodafone_czech_volte_vowifi  S174.1             909     lilac / poplar          Vodafone CZ
    231  02   telekom_slovakia_ims         S206.1             910     lilac / maple           T-Mobile SK
    234  10   telefonica_uk_ims            S23.1              911     lilac / maple / poplar  O2 Pay monthly UK
    234  94   hutch_uk_volte_vowifi        S21.2              912     maple                   3 UK
    260  02   telekom_poland_volte_vowifi  S8657.1            913     maple / poplar          T-Mobile PL
    260  06   play_poland_vowifi_only      S622.2 – S622.4    914     maple                   Play PL
    272  01   vodafone_global              S26.1              915     lilac / maple / poplar  Vodafone IE
    272  03   meteor_ireland_vowifi_only   S28.1              916     maple / poplar          Meteor IE
    320  490  freedom_ca_ims               S8738.1            917     poplar_canada           Freedom CA
    334  020  telcel_mx_ims                S52.1              918     maple / poplar          Telcel GSM MX
    425  03   pelephone_israel             S4508.1 – S4508.2  919     maple                   Pelephone IL
    716  10   claro_pe_ims                 S412.1             920     maple                   Claro PE
    722  07   movistar_ar_ims              S91.1              921     poplar                  Movistar AR
    722  310  claro_ar_ims                 S85.1              923     poplar                  Claro AR
    730  02   movistar_dtm_off             S401.1             924     maple                   Movistar CL
    732  123  movistar_co_ims              S405.1             925     poplar                  Movistar CO
    737  101  claro_co_ims                 S403.1             926     maple                   Comcel 3GSM CO
    740  00   movistar_dtm_off             S414.1             927     poplar                  Movistar EC

--- device/config-files.mk
+++ device/config-files.mk
@@ -8,4 +8,5 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/config/modem/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/modem.conf \
     $(DEVICE_PATH)/config/modem/287/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/287/modem.conf \
     $(DEVICE_PATH)/config/modem/409/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/409/modem.conf \
-    $(DEVICE_PATH)/config/modem/451/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/451/modem.conf
+    $(DEVICE_PATH)/config/modem/451/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/451/modem.conf \
+    $(DEVICE_PATH)/config/modem/917/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/917/modem.conf
