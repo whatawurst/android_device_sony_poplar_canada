47c56c9dcd1e9573078ca4b50bae3a1f81b3fd51
derfelot <derfelot.git@gmail.com>
2022-10-01 05:29:15 +0200

    poplar_canada: Update for Android 12.1
    Includes the following:
      * Rebrand omni --> twrp
      * Update prebuilt kernel from latest LineageOS 20.0 build
      * Update and add more required blobs from latest LineageOS 20.0 build
      * Switch init to common init.recovery.qcom_decrypt.rc
      * Add common qcom_decrypt packages
      * Update fstab and twrp.flags
      * Remove the now unneeded TW_INCLUDE_CRYPTO_FBE flag
      * Add a symlink to /persist to allow mounting of partition
      * Add msm8998 as PRODUCT_PLATFORM - not needed as a guard in yoshino-common anymore
      * Drop custom certificate inclusion
      * Update/add AOSP/TWRP configuration inherits
      * Drop unneeded TARGET_SCREEN_DENSITY
      * Move TARGET_COPY_OUT_VENDOR to yoshino-common - maple is trebelized
    
    Note: The manifest files from LineageOS 20.0 were downgraded to version 4.0 to be compatible with Android 12.1.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_poplar_canada.mk
+    $(LOCAL_DIR)/twrp_poplar_canada.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_poplar_canada-eng \
+    twrp_poplar_canada-userdebug
